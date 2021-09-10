087af2f3e1764128b9c8d2d4fd979f7792e25167
bradripple <bradripple@gmail.com>
2021-09-10 12:33:15 -0600

    Got submit recipe to add and display on profile

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -33,16 +33,51 @@ router.get('/details/:idx', async (req, res) => {
     }
 });
 
-router.post('/', async function(req, res) {
-    const { id } = req.user.get();
 
-    console.log('req', req.body);
-    let recipeId = req.body.addrecipe;
+// adding association
+router.post('/:id', async function(req, res) {
+    const { id } = req.user.get();
+    const recipeId = req.params.id;
+
+    // console.log('req', req.body);
+    // let recipeId = req.body.addrecipe;
     const addRecipe = await Userfav.create({ userId: id, recipeId})
 
     // fs.writeFileSync('./user-favs.json', JSON.stringify(cars));
 
     res.redirect('/profile');
+});
+
+
+//  actually adding the new recipe
+router.post('/', async function(req, res) {
+    try {
+        const { id } = req.user.get();
+    const {name, img_url, description, yields, prepTime, cookTime, totalTime, ingredients, instructions, tags, servings} = req.body;
+
+    const ingredientArr = [ingredients];
+    const instructionArr = [instructions];
+    const tagsArr = [tags];
+
+    const createdRecipe = await Recipe.create({name, img_url, description, yields, prepTime, cookTime, totalTime, ingredientArr, instructionArr, tagsArr, servings});
+
+    console.log(createdRecipe);
+
+    const addRecipe = await Userfav.create({ userId: id, recipeId: createdRecipe.id})
+
+    // console.log('req', req.body);
+    // let recipeId = req.body.addrecipe;
+    // const addRecipe = await Userfav.create({ userId: id, recipeId})
+
+    // fs.writeFileSync('./user-favs.json', JSON.stringify(cars));
+
+    res.redirect('/profile');
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+   
 });
 
 module.exports = router;
